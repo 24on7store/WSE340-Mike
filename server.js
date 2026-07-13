8c4925eda8b4a895cd9dc48fe42cb444209192f7
24on7store <132136928+24on7store@users.noreply.github.com>
2026-07-13 08:52:55 +0300

    w01 learning activities

---
+++ server.js
@@ -0,0 +1,60 @@
+import express from 'express';
+
+// line added in week1
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Define the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+// codes added in week 1??
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+//app.get('/', (req, res) => {
+  //res.send('Hello from Express!');
+//});
+
+// codes added in week 1
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Codes added in week 1
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+// Codes added in week 1
+/**
+  * Routes
+  */
+app.get('/', async (req, res) => {
+  const title = 'Home';
+    res.render('home', { title: 'Home Page' });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
