2a6842353011a83fde87d7899d8ea44790aad57c
24on7store <132136928+24on7store@users.noreply.github.com>
2026-07-16 01:34:18 +0300

    Feedback session 2

--- src/models/projects.js
+++ src/models/projects.js
@@ -6,7 +6,7 @@ import db from './db.js';
  */
 const getAllProjects = async () => {
     const query = `
-        SELECT project_id, title, description 
+        SELECT project_id, title, description, location, date, organization_id 
         FROM public.project 
         ORDER BY project_id ASC;
     `;
